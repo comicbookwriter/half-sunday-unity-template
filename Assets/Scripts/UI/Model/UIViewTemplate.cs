13e8d9dd0249b4fcde3fd163cf7fe26b7fd8cc01
CTAKnoll <ctk140030@utdallas.edu>
2023-12-27 21:34:02 -0800

    Templates now have their generic type restricted to UIInteractable

--- Assets/Scripts/UI/Model/UIViewTemplate.cs
+++ Assets/Scripts/UI/Model/UIViewTemplate.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace UI.Model
 {
-    public class UIViewTemplate<TView> : ScriptableObject
+    public class UIViewTemplate<TView> : ScriptableObject where TView : UIInteractable
     {
         public TView Prefab;
     }
